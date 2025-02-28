bcdb7d7a3fc25079adcaa5778798bb6d2d507d6e
1shan-pixel <ispanta03@gmail.com>
2025-02-28 22:52:25 +0545

    media query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,19 +9,19 @@ export default function Home() {
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
       {/* Content */}
-      <div className="flex flex-col items-center justify-center h-full relative">
-        <h1 className="text-3xl  text-white mb-4">Ishan Panta</h1>
-        <h2 className="text-3xl text-white mb-8">AI | Web Dev | Robotics</h2>
-        <div className="flex space-x-4">
+      <div className="flex flex-col items-center justify-center h-full relative px-4 text-center">
+        <h1 className="text-4xl md:text-5xl font-bold text-white mb-4">Ishan Panta</h1>
+        <h2 className="text-xl md:text-3xl text-white mb-8">AI | Web Dev | Robotics</h2>
+        <div className="flex space-x-6">
           {/* Social Media Icons */}
           <a href="https://twitter.com/1shanpanta" target="_blank" rel="noopener noreferrer">
-            <FaTwitter className="w-6 h-6 text-white hover:text-blue-40 transition-colors duration-300" />
+            <FaTwitter className="w-8 h-8 md:w-6 md:h-6 text-white hover:text-blue-400 transition-colors duration-300" />
           </a>
           <a href="https://www.linkedin.com/in/1shanpanta" target="_blank" rel="noopener noreferrer">
-            <FaLinkedinIn className="w-7 h-7 text-white hover:text-blue-60 transition-colors duration-300" />
+            <FaLinkedinIn className="w-8 h-8 md:w-7 md:h-7 text-white hover:text-blue-600 transition-colors duration-300" />
           </a>
           <a href="https://github.com/1shanpanta" target="_blank" rel="noopener noreferrer">
-            <FaGithub className="w-6 h-6 text-white hover:text-gray-70 transition-colors duration-300" />
+            <FaGithub className="w-8 h-8 md:w-6 md:h-6 text-white hover:text-gray-400 transition-colors duration-300" />
           </a>
         </div>
       </div>
